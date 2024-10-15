6380cfdbda23148151bf6120b5e4697a87f2465c
Ngô Vũ <ngovanvu083@gmail.com>
2024-10-15 16:00:22 +0700

    done footer

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import style from "./Logo.module.css";
-import logo from "./../assets/image/header/38309c2b-3bf5-49ce-ba9d-67b662f65b0b.jfif";
+import logo from "./../assets/image/header/logo.jfif";
 import { Link } from "react-router-dom";
 function Logo() {
   return (
