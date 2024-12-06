a705df9756371f1f4b0940d5746ac4b27538b49c
Ngô Vũ <ngovanvu083@gmail.com>
2024-12-07 01:47:40 +0700

    done header findBoat

--- src/pages/FindBoat/FindBoatDetailPage.jsx
+++ src/pages/FindBoat/FindBoatDetailPage.jsx
@@ -3,10 +3,18 @@ import React, { useEffect, useRef, useState } from "react";
 import styles from "./FindBoatDetailPage.module.css";
 import SliderDetail from "../../components/SliderDetail";
 import BoxFeatures from "../../components/Boxfeatures";
+import BreadCumbs from "../../components/BreadCumbs";
+import DetailPageHeader from "../../components/DetailPageHeader";
+import dataCardBoat from "../../data/dataCardBoat";
+import { useParams } from "react-router-dom";
 
 function FindBoatDetailPage() {
+  const { slug } = useParams();
+  const filterData = dataCardBoat.data.find((item) => slug === item.slug);
   return (
     <>
+      <BreadCumbs breadLink={"Tìm Du Thuyền"} filterData={filterData} />
+      <DetailPageHeader filterData={filterData} />
       <SliderDetail />
       <BoxFeatures />
     </>
