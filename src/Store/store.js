88b37baee719798ab825946099e2810ce0c8faf8
Ngô Vũ <ngovanvu083@gmail.com>
2024-12-26 18:58:49 +0700

    fix store

--- src/Store/store.js
+++ src/Store/store.js
@@ -1,11 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import cartSlice from "./boatroom/cartSlice";
-import toggleSlice from "./alltoggle/toggleSlice";
 
 const store = configureStore({
   reducer: {
     cart: cartSlice,
-    toggle: toggleSlice,
   },
 });
 export default store;
