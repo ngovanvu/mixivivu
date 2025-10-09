36ae994273a79e4c3e51ce22cbbdc4cf2fba8388
Ngô Vũ <ngovanvu083@gmail.com>
2025-10-09 19:37:41 +0700

    create login

--- src/components/HeaderUser.jsx
+++ src/components/HeaderUser.jsx
@@ -2,14 +2,16 @@ import React from "react";
 
 import styles from "./HeaderUser.module.css";
 import ToggleMenu from "./ToggleMenu";
-import ContactHeader from "./ContactHeader";
+// import ContactHeader from "./ContactHeader";
+import MenuUser from "./MenuUser";
+import AvatarUser from "./AvatarUser";
 
 function HeaderUser({ toggleMenu }) {
   return (
     <div className={styles.menu}>
-      {/* <AvatarUser /> */}
-      {/* <MenuUser /> */}
-      <ContactHeader />
+      <AvatarUser />
+      <MenuUser />
+      {/* <ContactHeader /> */}
       <ToggleMenu toggleMenu={toggleMenu} />
     </div>
   );
---
+++ src/pages/Login.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Login() {
+  return <div>logo
+  
+  <h4></h4>
+  </div>;
+}
+
+export default Login;
